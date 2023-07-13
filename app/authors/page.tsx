4f3eed349d4a8394c1062d54dc0042224f7c467b
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-07-13 13:58:10 +0300

    remove imports

--- app/authors/page.tsx
+++ app/authors/page.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-
 export default function Authors() {
   return (
     <div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 export const preferredRegion = "home";
 export const dynamic = "force-dynamic";
